64d29d0e31894f0fd344bcd8e1afebd291e35bb1
Robin Luiten <robin.luiten+git@gmail.com>
2016-09-02 20:01:59 +1000

    feat(locale): integrate Romanian translation. v8.1.0

--- src/Date/Extra/Config/Configs.elm
+++ src/Date/Extra/Config/Configs.elm
@@ -22,6 +22,7 @@ import Date.Extra.Config.Config_en_gb as Config_en_gb
 import Date.Extra.Config.Config_fr_fr as Config_fr_fr
 import Date.Extra.Config.Config_pl_pl as Config_pl_pl
 import Date.Extra.Config.Config_fi_fi as Config_fi_fi
+import Date.Extra.Config.Config_ro_ro as Config_ro_ro
 
 
 {-| Built in configurations. -}
@@ -34,6 +35,7 @@ configs =
     , ("fr_fr", Config_fr_fr.config)
     , ("pl_pl", Config_pl_pl.config)
     , ("fi_fi", Config_fi_fi.config)
+    , ("ro_ro", Config_ro_ro.config)
     ]
 
 
--- tests/Date/Extra/ConfigTests.elm
+++ tests/Date/Extra/ConfigTests.elm
@@ -7,6 +7,8 @@ import Date.Extra.Config.Config_en_au as Config_en_au
 import Date.Extra.Config.Config_en_us as Config_en_us
 import Date.Extra.Config.Config_fr_fr as Config_en_gb
 import Date.Extra.Config.Config_fr_fr as Config_fr_fr
+import Date.Extra.Config.Config_pl_pl as Config_pl_pl
+import Date.Extra.Config.Config_ro_ro as Config_ro_ro
 import Date.Extra.Config.Configs as Configs
 
 
@@ -14,6 +16,8 @@ config_en_au = Config_en_au.config
 config_en_us = Config_en_us.config
 config_en_gb = Config_en_gb.config
 config_fr_fr = Config_fr_fr.config
+config_pl_pl = Config_pl_pl.config
+config_ro_ro = Config_ro_ro.config
 
 
 tests : Test
@@ -43,4 +47,12 @@ tests =
         assertEqual
           config_fr_fr.format
           (Configs.getConfig "fr_fr").format
+    , test "getConfig ro_ro" <|
+        assertEqual
+          config_pl_pl.format
+          (Configs.getConfig "pl_pl").format
+    , test "getConfig pl_pl" <|
+        assertEqual
+          config_ro_ro.format
+          (Configs.getConfig "ro_ro").format
     ]
