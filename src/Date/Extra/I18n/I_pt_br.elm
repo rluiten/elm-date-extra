e19af2b59b6b3cc969c61d49f85ec43a20d58301
Robin <robin.luiten+github@gmail.com>
2017-01-07 10:09:22 +1000

    style(I_pt_br): change formatting to match other translation files.

--- src/Date/Extra/I18n/I_pt_br.elm
+++ src/Date/Extra/I18n/I_pt_br.elm
@@ -18,138 +18,66 @@ import String exposing (padLeft)
 -}
 dayShort : Day -> String
 dayShort day =
-    case day of
-        Mon ->
-            "Seg"
-
-        Tue ->
-            "Ter"
-
-        Wed ->
-            "Qua"
-
-        Thu ->
-            "Qui"
-
-        Fri ->
-            "Sex"
-
-        Sat ->
-            "Sáb"
-
-        Sun ->
-            "Dom"
+  case day of
+    Mon -> "Seg"
+    Tue -> "Ter"
+    Wed -> "Qua"
+    Thu -> "Qui"
+    Fri -> "Sex"
+    Sat -> "Sáb"
+    Sun -> "Dom"
 
 
 {-| Day full name.
 -}
 dayName : Day -> String
 dayName day =
-    case day of
-        Mon ->
-            "Segunda-feira"
-
-        Tue ->
-            "Terça-feira"
-
-        Wed ->
-            "Quarta-feira"
-
-        Thu ->
-            "Quinta-feira"
-
-        Fri ->
-            "Sexta-feira"
-
-        Sat ->
-            "Sábado"
-
-        Sun ->
-            "Domingo"
+  case day of
+    Mon -> "Segunda-feira"
+    Tue -> "Terça-feira"
+    Wed -> "Quarta-feira"
+    Thu -> "Quinta-feira"
+    Fri -> "Sexta-feira"
+    Sat -> "Sábado"
+    Sun -> "Domingo"
 
 
 {-| Month short name.
 -}
 monthShort : Month -> String
 monthShort month =
-    case month of
-        Jan ->
-            "Jan"
-
-        Feb ->
-            "Fev"
-
-        Mar ->
-            "Mar"
-
-        Apr ->
-            "Abr"
-
-        May ->
-            "Mai"
-
-        Jun ->
-            "Jun"
-
-        Jul ->
-            "Jul"
-
-        Aug ->
-            "Ago"
-
-        Sep ->
-            "Set"
-
-        Oct ->
-            "Out"
-
-        Nov ->
-            "Nov"
-
-        Dec ->
-            "Dez"
+  case month of
+    Jan -> "Jan"
+    Feb -> "Fev"
+    Mar -> "Mar"
+    Apr -> "Abr"
+    May -> "Mai"
+    Jun -> "Jun"
+    Jul -> "Jul"
+    Aug -> "Ago"
+    Sep -> "Set"
+    Oct -> "Out"
+    Nov -> "Nov"
+    Dec -> "Dez"
 
 
 {-| Month full name.
 -}
 monthName : Month -> String
 monthName month =
-    case month of
-        Jan ->
-            "Janeiro"
-
-        Feb ->
-            "Fevereiro"
-
-        Mar ->
-            "Março"
-
-        Apr ->
-            "Abril"
-
-        May ->
-            "Maio"
-
-        Jun ->
-            "Junho"
-
-        Jul ->
-            "Julho"
-
-        Aug ->
-            "Agosto"
-
-        Sep ->
-            "Setembro"
-
-        Oct ->
-            "Outubro"
-
-        Nov ->
-            "Novembro"
-
-        Dec ->
-            "Dezembro"
+  case month of
+    Jan -> "Janeiro"
+    Feb -> "Fevereiro"
+    Mar -> "Março"
+    Apr -> "Abril"
+    May -> "Maio"
+    Jun -> "Junho"
+    Jul -> "Julho"
+    Aug -> "Agosto"
+    Sep -> "Setembro"
+    Oct -> "Outubro"
+    Nov -> "Novembro"
+    Dec -> "Dezembro"
 
 
 {-| Returns a common Brazilian Portuguse idiom for days of month. Pad indicates
@@ -158,16 +86,13 @@ make them same length as double digit days of month.
 -}
 dayOfMonthWithSuffix : Bool -> Int -> String
 dayOfMonthWithSuffix pad day =
-    let
-        value =
-            case day of
-                1 ->
-                    "1º"
-
-                _ ->
-                    toString day
-    in
-        if pad then
-            padLeft 4 ' ' value
-        else
-            value
+  let
+    value =
+      case day of
+        1 -> "1º"
+        _ -> toString day
+  in
+    if pad then
+      padLeft 4 ' ' value
+    else
+      value
