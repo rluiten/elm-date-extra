c2677fc840264f70f6911e109f49939d6e4639a2
jinjor <jinjorweb@gmail.com>
2017-02-16 19:29:05 +0900

    add locale for Japanese(ja_jp)

---
+++ src/Date/Extra/Config/Config_ja_jp.elm
@@ -0,0 +1,33 @@
+module Date.Extra.Config.Config_ja_jp exposing (..)
+
+{-| This is the Japanese config for formatting dates.
+
+@docs config
+
+Copyright (c) 2017 Yosuke Torii
+-}
+
+import Date
+import Date.Extra.Config as Config
+import Date.Extra.I18n.I_ja_jp as Japanese
+
+
+{-| Config for ja_jp. -}
+config : Config.Config
+config =
+  { i18n =
+      { dayShort = Japanese.dayShort
+      , dayName = Japanese.dayName
+      , monthShort = Japanese.monthShort
+      , monthName = Japanese.monthName
+      , dayOfMonthWithSuffix = Japanese.dayOfMonthWithSuffix
+      }
+  , format =
+      { date = "%Y/%m/%d" -- yyyy/MM/dd
+      , longDate = "%Y年%m月%d(%a)" -- yyyy年MM月dd日(aaa)
+      , time = "%H:%M" -- hh:mm
+      , longTime = "%H時%M分%S秒" -- hh:mm:ss
+      , dateTime = "%Y/%m/%d %H:%M"  -- date + time
+      , firstDayOfWeek = Date.Mon
+      }
+  }
